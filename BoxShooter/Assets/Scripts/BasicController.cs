9a35f0a6e358c641b72faa37276392e41daf6907
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-02-17 19:22:08 +0100

    Initial commit

---
+++ BoxShooter/Assets/Scripts/BasicController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Horizontal Input = " + Input.GetAxis("Horizontal"));
+	}
+}
